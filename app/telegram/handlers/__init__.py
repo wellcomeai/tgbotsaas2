63d19a325c0572eb22980aad25c66ed7224fd489
wellcomeai <19wellai96@gmail.com>
2025-08-03 12:14:36 +0300

    Create __init__.py

---
+++ app/telegram/handlers/__init__.py
@@ -0,0 +1,8 @@
+"""
+Telegram Handlers
+"""
+
+from app.telegram.handlers.master_bot import MasterBotHandler
+from app.telegram.handlers.user_bot import UserBotHandler
+
+__all__ = ["MasterBotHandler", "UserBotHandler"]
