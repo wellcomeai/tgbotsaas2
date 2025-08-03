c2639a3db38667a7afd2036e4788aaf776c121b5
wellcomeai <19wellai96@gmail.com>
2025-08-03 12:12:20 +0300

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+"""
+Telegram Bot Factory - SaaS платформа для создания и управления Telegram ботов
+"""
+
+__version__ = "2.0.0"
+__author__ = "Bot Factory Team"
+__description__ = "Единая платформа для управления Telegram ботами"
